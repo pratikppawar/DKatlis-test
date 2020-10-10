ba7c5c6cb06ecfee2694c37015a558ddbb90fab4
Pawar <pratik.pawar@sap.com>
2020-10-10 14:28:34 +0530

    Update App class

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,10 +1,25 @@
 package app;
 
 import app.models.TicketManager;
+import app.models.TicketManagerImpl;
 
 public class App {
+	
 	private TicketManager ticketManagerObj;
+	
+	public App() {
+		ticketManagerObj = new TicketManagerImpl();
+	}
+	
 	public static void main(String[] args) {
-		
+		App appObj = new App();
+	}
+
+	public TicketManager getTicketManagerObj() {
+		return ticketManagerObj;
+	}
+
+	public void setTicketManagerObj(TicketManager ticketManagerObj) {
+		this.ticketManagerObj = ticketManagerObj;
 	}
 }
